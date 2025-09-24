aa96c5fe0a8a071ee13b5b86234c736310973113
s-banerjee94 <shondha.tara94@gmail.com>
2025-09-24 23:28:36 +0530

    Add useranswer-service module and initial configuration

---
+++ useranswer-service/src/main/java/in/connectwithsandeepan/interviewgenius/useranswerservice/UseranswerServiceApplication.java
@@ -0,0 +1,13 @@
+package in.connectwithsandeepan.interviewgenius.useranswerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseranswerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseranswerServiceApplication.class, args);
+	}
+
+}
---
+++ useranswer-service/src/test/java/in/connectwithsandeepan/interviewgenius/useranswerservice/UseranswerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.connectwithsandeepan.interviewgenius.useranswerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseranswerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
