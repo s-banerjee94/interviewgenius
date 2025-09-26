c7cc1832c6b5139b760515c95159ba6fd3754021
s-banerjee94 <shondha.tara94@gmail.com>
2025-09-27 00:26:06 +0530

    Refactor AiServiceClient to remove hardcoded URL for better configuration management

--- question-service/src/main/java/in/connectwithsandeepan/interviewgenius/questionservice/client/AiServiceClient.java
+++ question-service/src/main/java/in/connectwithsandeepan/interviewgenius/questionservice/client/AiServiceClient.java
@@ -4,7 +4,7 @@ import in.connectwithsandeepan.interviewgenius.questionservice.entity.Question;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "ai-service", url = "http://localhost:8083", path = "/api/v1/ai")
+@FeignClient(name = "ai-service", path = "api/v1/ai")
 public interface AiServiceClient {
 
     @GetMapping("/question")
